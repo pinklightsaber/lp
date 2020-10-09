a3d2f35f5332b602a0fea89e3b3e737505bd0bd0
pinklightsaber <ctorres@idemar.cl>
2020-10-09 00:51:57 -0300

    m

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+
+    $(window).on('scroll', function () {
+        var windowHeight = $(window).height(),
+            gridTop = windowHeight * .01,
+            gridBottom = windowHeight * 1.1;
+        $('body section div').each(function () {
+            var thisTop = $(this).offset().top - $(window).scrollTop();
+
+            if (thisTop > gridTop && (thisTop + $(this).height()) < gridBottom) {
+                $(this).addClass("change");
+            } else {
+                $(this).removeClass("change");
+            }
+        });
+
+        var $nav = $(".nav-wrapper");
+        var $sec = $("section");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $sec.height());
+ 
+
+    });
+    $(window).trigger('scroll');
+
+
+ 
+    var masonry = new Macy({
+        container: '#macy-container',
+        trueOrder: false,
+        waitForImages: false,
+        useOwnImageLoader: false,
+        debug: true,
+        mobileFirst: true,
+        columns: 1,
+        margin: {
+            y: 16,
+            x: '2%',
+        },
+        breakAt: {
+            1200: 5,
+            940: 4,
+            520: 2,
+            400: 1
+        },
+    });
